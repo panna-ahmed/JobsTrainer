d548099ecd25b167a4c8ed9d3c7e295419197560
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2022-08-19 09:07:13 +0600

    Skill Api

---
+++ JobsTrainer/Controllers/api/SkillsController.cs
@@ -0,0 +1,26 @@
+﻿using JobsTrainer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobsTrainer.Controllers.Api
+{
+    [ApiController]
+    [Route("api/skills")]
+    public class SkillsController : Controller
+    {
+        private readonly TrainingContext _context;
+
+        public SkillsController(TrainingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var skills = await _context.Skill.Select(c => c.Name).ToArrayAsync();
+
+            return Ok(skills);
+        }
+    }
+}
